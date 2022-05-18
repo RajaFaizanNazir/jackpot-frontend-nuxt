5a5502164c62f1f6bca3dd0a61a43576ec25e430
Raja Faizan Nazir <RFaizanN@gmail.com>
2022-05-18 11:47:20 +0500

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'mytoken',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel:"stylesheet", href:"https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    // '@nuxtjs/tailwindcss',
+
+    // 'tailwindcss-color-mode'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+
+
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+  purgeCSS: {
+    whitelist: ['dark-mode']
+    },
+
+
+}
---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 text-center">
+          <h1>{{fetchedBalance}}</h1>
+
+        </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+          <div class="form-group">
+               <label>Enter the Receiver Address</label>
+               <input class="form-control" type="text" v-model="receiverAddress">
+          </div>
+          <div class="form-group">
+            <label>Enter Amount</label>
+            <input class="form-control" type="text" v-model="amount">
+
+          </div>
+      </div>
+    <div class="form-group text-center">
+      <button @click="transfertoken()" class="btn btn-primary  ">
+        Send Amount
+      </button>
+    </div>
+
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+import abi from "../Abi/abi";
+import { ethers } from "ethers";
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      contractAddress: "0xA0f10C9A521621f54228777aCC4B15baDE86c2FA", // Contract Address.
+      fetchedBalance: "",
+      receiverAddress:"",
+      amount:""
+      // balance: this.balance,
+    };
+  },
+  async mounted() {
+    this.totalbalance()
+    // this.transfertoken()
+  },
+
+  methods: {
+    async totalbalance() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const myToken = new ethers.Contract(this.contractAddress, abi, provider);
+      const balance = await myToken.balanceOf("0xE6a60007AE143532BBdd03a8D711f8e146A127F0");
+      this.fetchedBalance = balance;
+      console.log(balance);
+    },
+    async transfertoken() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await window.ethereum.enable();
+      const signer = await provider.getSigner();
+      //  console.log(await provider.getSigner().getAddress())
+      const myToken = new ethers.Contract(this.contractAddress, abi, signer);
+      const transfer = await myToken.transfer(this.receiverAddress,this.amount);
+      // await axios.post('localhost:5000/')
+      console.log(transfer);
+    },
+
+
+  }
+};
+</script>
